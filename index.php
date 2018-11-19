ebef15a13c32bf11cade3bb13453f806c079e0b1
Rahul Agarwal <agarwal.rahul324@gmail.com>
2018-11-19 20:06:59 +0530

    //

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $user=Login::isLoggedIn();
     <script src="./js/preloader.js"></script>
     <script src='js/jquery.ba-hashchange.min.js'></script>
     <link rel="shortcut icon" type="image/x-icon" href="./img/favicon.ico" />
-    <link rel="stylesheet" href="./css/all.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <script src='js/dynamicpage.js'></script>
     <title>Digital Literacy Programme</title>
 </head>
