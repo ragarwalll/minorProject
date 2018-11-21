d7411ff926d33be1e4835c83381182803c4d7e32
Rahul Agarwal <agarwal.rahul324@gmail.com>
2018-11-21 21:05:07 +0530

    Main pages-levels

--- intermediate/initial.php
+++ intermediate/initial.php
@@ -4,11 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/main.css">
     <link rel="stylesheet" href="../css-global/preloader-icon.css">
     <script type='text/javascript' src='../js-global/jquery.min.js'></script>
     <script src='../js-global/preloader-icon.js'></script>
+    <link rel="stylesheet" href="../css-global/header.css">
     <script src='../js-global/header-toggle.js'></script>
+    <link rel="stylesheet" href="../css-global/left-sided.css">
+    <link rel="stylesheet" href="../css-global/main-wrapper.css">
+    <link rel="stylesheet" href="../css-global/footer-fixed.css">
     <title>Intermediate | Digital Literacy Programme</title>
 </head>
 <body>
@@ -26,5 +31,44 @@
             <div class="pencil_h"></div>
         </div>
     </div>
+
+    <div id="container">    
+        <header>
+            <div class="container" id="nav">
+            <div class="logo"><a href="index.php"> <img src="../img/logo.png" width="30" alt=""></div></a>
+                <nav>
+                    <ul>
+                    <li><a href="" class="navItem navItem1">HOME</a></li>
+                    <li><a href="" class="navItem">DASHBOARD</a></li>
+                    <li><a href="" class="navItem">CONTACT</a></li>
+                    <li><a href="" class="navItem">CONTRIBUTORS</a></li>
+                    </ul>
+                </nav>
+                <div class="nav-toggle">
+                    <div class="hamburger"></div>
+                </div>
+                <div class="nav-toggle1">
+                        <div class="hamburger1"></div>
+                </div>
+            </div>
+        </header>
+        <div class="paging">
+            <a href="" class="pageInc"><i class="fas fa-chevron-circle-left ico"></i> Previous Course</a>
+            <a href="" class="pageInc pageInc1">Next Course <i class="fas fa-chevron-circle-right ico"></i></a>
+        </div>
+
+        <div class="lef-sided">
+            <div class="index">
+                <a href="2.html">1</a><br>
+                <a href="">2</a><br>
+                <a href="">3</a><br>
+                <a href="">4</a><br>
+                <a href="">5</a><br>
+                </div>
+        </div>
+
+        <div class="main-wrapper">
+            <p id="starting">Hi</p>
+        </div>
 </body>
 </html>
